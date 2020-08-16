5779e2c075d130dc6519b68bca28a3aad67fb742
Sumit Yadav <53750345+YadavSumit2408@users.noreply.github.com>
2020-08-16 13:14:58 +0530

    Add files via upload

---
+++ category/constant.dart
@@ -0,0 +1,42 @@
+const MAIN_DATA = [
+  {
+    "name":"HOME",
+    "price":"₹ 10/100 sq.ft" ,
+    "image":"1.png"
+  },{
+    "name":"Flat",
+    "price":"₹ 10/100 sq.ft",
+    "image":"1.png"
+  },
+  {
+    "name":"Bungalow",
+    "price":"₹ 10/100 sq.ft",
+    "image":"1.png"
+  },
+  {
+    "name":"Shop",
+    "price":"₹ 10/100 sq.ft",
+    "image":"2.png"
+  },
+  {
+    "name":"Offices",
+    "price":"₹ 10/100 sq.ft",
+    "image":"2.png"
+  },
+  {
+    "name":" Apartment Buildings or Complex",
+    "price":"₹ 10/100 sq.ft",
+    "image":"2.png"
+  },
+  {
+    "name":"Educational Firms(Schools,Coachings or Institutes)",
+    "price":"₹ 10/100 sq.ft",
+    "image":"2.png"
+  },
+  {
+    "name":" Others(Streets,Areas etc.)",
+    "price":"₹ 10/100 sq.ft",
+    "image":"2.png"
+  },
+  
+];
